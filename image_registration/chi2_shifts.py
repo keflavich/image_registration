6b1649b63fc3cfa050e164dbc1fc506a4c18bc23
Adam <keflavich@gmail.com>
2012-09-02 19:07:09 -0600

    Added separate image registration techniques Added testing code

---
+++ image_registration/chi2_shifts.py
@@ -0,0 +1,302 @@
+try: 
+    from AG_fft_tools import correlate2d,fast_ffts
+except ImportError:
+    from fft_tools import correlate2d,fast_ffts
+import warnings
+import numpy as np
+
+def chi2_shift(im1, im2, err=None, upsample_factor=10, boundary='wrap',
+        nthreads=1, use_numpy_fft=False, zeromean=False, ndof=2, verbose=True,
+        return_error=True, return_chi2array=False):
+    """
+    Find the offsets between image 1 and image 2 using the DFT upsampling method
+    (http://www.mathworks.com/matlabcentral/fileexchange/18401-efficient-subpixel-image-registration-by-cross-correlation/content/html/efficient_subpixel_registration.html)
+    combined with chi^2 to measure the errors on the fit
+    
+    Parameters
+    ----------
+    im1 : np.ndarray
+    im2 : np.ndarray
+        The images to register. 
+    err : np.ndarray
+        Per-pixel error in image 2
+    boundary : 'wrap','constant','reflect','nearest'
+        Option to pass to map_coordinates for determining what to do with
+        shifts outside of the boundaries.  
+    upsample_factor : int
+        upsampling factor; governs accuracy of fit (1/usfac is best accuracy)
+    return_error : bool
+        Returns the "fit error" (1-sigma in x and y) based on the delta-chi2
+        values
+    return_chi2_array : bool
+        Returns the x and y shifts and the chi2 as a function of those shifts
+        in addition to other returned parameters.  i.e., the last return from
+        this function will be a tuple (x, y, chi2)
+    zeromean : bool
+        Subtract the mean from the images before cross-correlating?  If no, you
+        may get a 0,0 offset because the DC levels are strongly correlated.
+    verbose : bool
+        Print error message if upsampling factor is inadequate to measure errors
+    use_numpy_fft : bool
+        Force use numpy's fft over fftw?  (only matters if you have fftw
+        installed)
+    nthreads : bool
+        Number of threads to use for fft (only matters if you have fftw
+        installed)
+    ndof : int
+        number of degrees of freedom in the fit (used for chi^2 computations).
+        Should probably always be 2.
+
+
+    Returns
+    -------
+    dx,dy : float,float
+        REVERSE of dftregistration order (also, signs flipped) for consistency
+        with other routines.
+        Measures the amount im2 is offset from im1 (i.e., shift im2 by these #'s
+        to match im1)
+
+
+    """
+    if not im1.shape == im2.shape:
+        raise ValueError("Images must have same shape.")
+
+    if zeromean:
+        im1 = im1 - (im1[im1==im1].mean())
+        im2 = im2 - (im2[im2==im2].mean())
+
+    if np.any(np.isnan(im1)):
+        im1 = im1.copy()
+        im1[im1!=im1] = 0
+    if np.any(np.isnan(im2)):
+        im2 = im2.copy()
+        im2[im2!=im2] = 0
+
+    xc = correlate2d(im1,im2, boundary=boundary)
+    if err is not None:
+        err_ac = correlate2d(err,err, boundary=boundary)
+        err2sum = (err**2).sum()
+    else:
+        err_ac = xc.size - ndof
+        err2sum = xc.size - ndof
+    ac1peak = (im1**2).sum()
+    ac2peak = (im2**2).sum()
+    chi2n = (ac1peak/err2sum - 2*xc/err_ac + ac2peak/err2sum)
+    ymax, xmax = np.unravel_index(chi2n.argmin(), chi2n.shape)
+
+    fftn,ifftn = fast_ffts.get_ffts(nthreads=nthreads, use_numpy_fft=use_numpy_fft)
+
+    # biggest scale = where chi^2/n ~ 9?
+
+    s1,s2 = im1.shape
+    ylen,xlen = im1.shape
+    xcen = xlen/2-(1-xlen%2) 
+    ycen = ylen/2-(1-ylen%2) 
+    yshift = ymax-ycen # shift im2 by these numbers to get im1
+    xshift = xmax-xcen
+
+    #xcft = correlate2d(im1,im2,boundary=boundary,return_fft=True)
+
+    # signs?!
+    zoom_factor = s1/upsample_factor
+    if zoom_factor <= 1:
+        zoom_factor = 2
+        s1 = zoom_factor*upsample_factor
+        s2 = zoom_factor*upsample_factor
+    dftshift = np.trunc(np.ceil(upsample_factor*zoom_factor)/2); #% Center of output array at dftshift+1
+    xc_ups = dftups(fftn(im2)*np.conj(fftn(im1)), s1, s2, usfac=upsample_factor,
+            roff=dftshift-yshift*upsample_factor,
+            coff=dftshift-xshift*upsample_factor) / (im1.size) #*upsample_factor**2)
+    if err is not None:
+        err_ups = upsample_image(err, output_size=s1,
+                upsample_factor=upsample_factor, xshift=xshift, yshift=yshift)
+    else:
+        err_ups = 1
+    chi2n_ups = (ac1peak/err2sum-2*np.abs(xc_ups)/np.abs(err_ups)+ac2peak/err2sum)
+    deltachi2 = chi2n_ups - chi2n_ups.min()
+
+    yy,xx = np.indices([s1,s2])
+    xshifts_corrections = (xx-dftshift)/upsample_factor
+    yshifts_corrections = (yy-dftshift)/upsample_factor
+
+    try:
+        import scipy.stats
+        # 1,2,3-sigma delta-chi2 levels
+        m1 = scipy.stats.chi2.ppf( 1-scipy.stats.norm.sf(1)*2, ndof )
+        m2 = scipy.stats.chi2.ppf( 1-scipy.stats.norm.sf(2)*2, ndof )
+        m3 = scipy.stats.chi2.ppf( 1-scipy.stats.norm.sf(3)*2, ndof )
+    except ImportError:
+        # assume m=2 (2 degrees of freedom)
+        m1 = 2.2957489288986364
+        m2 = 6.1800743062441734 
+        m3 = 11.829158081900793
+
+    sigma1_area = deltachi2<m1
+    x_sigma1 = xshifts_corrections[sigma1_area]
+    y_sigma1 = yshifts_corrections[sigma1_area]
+    # optional...?
+    #sigma2_area = deltachi2<m2
+    #x_sigma2 = xshifts_corrections[sigma2_area]
+    #y_sigma2 = yshifts_corrections[sigma2_area]
+    #sigma3_area = deltachi2<m3
+    #x_sigma3 = xshifts_corrections[sigma3_area]
+    #y_sigma3 = yshifts_corrections[sigma3_area]
+
+    if sigma1_area.sum() <= 1:
+        if verbose:
+            print "Cannot estimate errors: need higher upsample factor"
+        errx_low = erry_low = errx_high = erry_high = 1./upsample_factor
+
+    upsymax,upsxmax = np.unravel_index(chi2n_ups.argmin(), chi2n_ups.shape)
+
+    errx_low = (upsxmax-dftshift)/upsample_factor - x_sigma1.min()
+    errx_high = x_sigma1.max() - (upsxmax-dftshift)/upsample_factor
+    erry_low = (upsymax-dftshift)/upsample_factor - y_sigma1.min()
+    erry_high = y_sigma1.max() - (upsymax-dftshift)/upsample_factor
+
+    yshift_corr = yshift+(upsymax-dftshift)/float(upsample_factor)
+    xshift_corr = xshift+(upsxmax-dftshift)/float(upsample_factor)
+    if verbose > 1:
+        #print ymax,xmax
+        #print upsymax, upsxmax
+        #print upsymax-dftshift, upsxmax-dftshift
+        print "Correction: ",(upsymax-dftshift)/float(upsample_factor), (upsxmax-dftshift)/float(upsample_factor)
+        print "Chi2 1sig bounds:", x_sigma1.min(), x_sigma1.max(), y_sigma1.min(), y_sigma1.max()
+        print errx_low,errx_high,erry_low,erry_high
+        print "%0.3f +%0.3f -%0.3f   %0.3f +%0.3f -%0.3f" % (yshift_corr, erry_high, erry_low, xshift_corr, errx_high, errx_low)
+        #print ymax-ycen+upsymax/float(upsample_factor), xmax-xcen+upsxmax/float(upsample_factor)
+        #print (upsymax-s1/2)/upsample_factor, (upsxmax-s2/2)/upsample_factor
+
+    shift_xvals = xshifts_corrections+xshift
+    shift_yvals = yshifts_corrections+yshift
+
+    returns = [xshift_corr,yshift_corr]
+    if return_error:
+        returns.append( (errx_low+errx_high)/2. )
+        returns.append( (erry_low+erry_high)/2. )
+    if return_chi2array:
+        returns.append((shift_xvals,shift_yvals,chi2n_ups))
+
+    return returns
+
+
+def chi2_shift_leastsq(im1, im2, err=None, mode='wrap', maxoff=None, return_error=True,
+        guessx=0, guessy=0, use_fft=False, ignore_outside=True, **kwargs):
+    """
+    Determine the best fit offset using `scipy.ndimage.map_coordinates` to
+    shift the offset image.
+    *OBSOLETE* It kind of works, but is sensitive to input guess and doesn't reliably
+    output errors
+
+    Parameters
+    ----------
+        im1 : np.ndarray
+            First image
+        im2 : np.ndarray
+            Second image (offset image)
+        err : np.ndarray
+            Per-pixel error in image 2
+        mode : 'wrap','constant','reflect','nearest'
+            Option to pass to map_coordinates for determining what to do with
+            shifts outside of the boundaries.  
+        maxoff : None or int
+            If set, crop the data after shifting before determining chi2
+            (this is a good thing to use; not using it can result in weirdness
+            involving the boundaries)
+
+    """
+    #xc = correlate2d(im1,im2, boundary=boundary)
+    #ac1peak = (im1**2).sum()
+    #ac2peak = (im2**2).sum()
+    #chi2 = ac1peak - 2*xc + ac2peak
+
+
+    if not im1.shape == im2.shape:
+        raise ValueError("Images must have same shape.")
+
+    if np.any(np.isnan(im1)):
+        im1 = im1.copy()
+        im1[im1!=im1] = 0
+    if np.any(np.isnan(im2)):
+        im2 = im2.copy()
+        if err is not None:
+            err[im2!=im2] = np.inf
+        im2[im2!=im2] = 0
+
+    im1 = im1-im1.mean()
+    im2 = im2-im2.mean()
+    if not use_fft:
+        yy,xx = np.indices(im1.shape)
+    ylen,xlen = im1.shape
+    xcen = xlen/2-(1-xlen%2) 
+    ycen = ylen/2-(1-ylen%2) 
+    import scipy.ndimage,scipy.optimize
+
+    def residuals(p, **kwargs):
+        xsh,ysh = p
+        if use_fft:
+            shifted_img = shift(im2, -ysh, -xsh)
+        else:
+            shifted_img = scipy.ndimage.map_coordinates(im2, [yy+ysh,xx+xsh], mode=mode, **kwargs)
+        if maxoff is not None:
+            xslice = slice(xcen-maxoff,xcen+maxoff,None)
+            yslice = slice(ycen-maxoff,ycen+maxoff,None)
+            # divide by sqrt(number of samples) = sqrt(maxoff**2)
+            residuals = np.abs(np.ravel((im1[yslice,xslice]-shifted_img[yslice,xslice])) / maxoff)
+        else:
+            if ignore_outside:
+                outsidex = min([(xlen-2*xsh)/2,xcen])
+                outsidey = min([(ylen-2*ysh)/2,xcen])
+                xslice = slice(xcen-outsidex,xcen+outsidex,None)
+                yslice = slice(ycen-outsidey,ycen+outsidey,None)
+                residuals = ( np.abs( np.ravel(
+                    (im1[yslice,xslice]-shifted_img[yslice,xslice]))) /
+                    (2*outsidex*2*outsidey)**0.5 )
+            else:
+                xslice = slice(None)
+                yslice = slice(None)
+                residuals = np.abs(np.ravel((im1-shifted_img))) / im1.size**0.5
+        if err is None:
+            return residuals
+        else:
+            if use_fft:
+                shifted_err = shift(err, -ysh, -xsh)
+            else:
+                shifted_err = scipy.ndimage.map_coordinates(err, [yy+ysh,xx+xsh], mode=mode, **kwargs)
+            return residuals / shifted_err[yslice,xslice].flat
+
+    bestfit,cov,info,msg,ier = scipy.optimize.leastsq(residuals, [guessx,guessy], full_output=1)
+    #bestfit,cov = scipy.optimize.curve_fit(shift, im2, im1, p0=[guessx,guessy], sigma=err)
+
+    chi2n = (residuals(bestfit)**2).sum() / (im1.size-2)
+
+    if bestfit is None or cov is None:
+        print bestfit-np.array([guessx,guessy])
+        print bestfit
+        print cov
+        print info
+        print msg
+        print ier
+        if cov is None:
+            from numpy.dual import inv
+            from numpy.linalg import LinAlgError
+            n = 2 # number of free pars
+            perm = np.take(np.eye(n),info['ipvt']-1,0)
+            r = np.triu(np.transpose(info['fjac'])[:n,:])
+            R = np.dot(r, perm)
+            try:
+                cov = inv(np.dot(np.transpose(R),R))
+            except LinAlgError:
+                print "Could not compute cov because of linalgerr"
+                pass
+                
+        
+    if return_error:
+        if cov is None:
+            return bestfit[0],bestfit[1],0,0
+        else: # based on scipy.optimize.curve_fit, the "correct" covariance is this cov * chi^2/n
+            return bestfit[0],bestfit[1],(cov[0,0]*chi2n)**0.5,(cov[1,1]*chi2n)**0.5
+    else:
+        return bestfit[0],bestfit[1]
+
+
---
+++ image_registration/register_images.py
@@ -0,0 +1,325 @@
+try: 
+    from AG_fft_tools import correlate2d,fast_ffts
+except ImportError:
+    from fft_tools import correlate2d,fast_ffts
+import warnings
+import numpy as np
+
+def register_images(im1, im2, usfac=1, return_registered=False,
+        return_error=False, zeromean=True, DEBUG=False, maxoff=None,
+        nthreads=1, use_numpy_fft=False):
+    """
+    Sub-pixel image registration (see dftregistration for lots of details)
+
+    Parameters
+    ----------
+    im1 : np.ndarray
+    im2 : np.ndarray
+        The images to register. 
+    usfac : int
+        upsampling factor; governs accuracy of fit (1/usfac is best accuracy)
+    return_registered : bool
+        Return the registered image as the last parameter
+    return_error : bool
+        Does nothing at the moment, but in principle should return the "fit
+        error" (it does nothing because I don't know how to compute the "fit
+        error")
+    zeromean : bool
+        Subtract the mean from the images before cross-correlating?  If no, you
+        may get a 0,0 offset because the DC levels are strongly correlated.
+    maxoff : int
+        Maximum allowed offset to measure (setting this helps avoid spurious
+        peaks)
+    DEBUG : bool
+        Test code used during development.  Should DEFINITELY be removed.
+
+    Returns
+    -------
+    dx,dy : float,float
+        REVERSE of dftregistration order (also, signs flipped) for consistency
+        with other routines.
+        Measures the amount im2 is offset from im1 (i.e., shift im2 by these #'s
+        to match im1)
+
+    """
+    if not im1.shape == im2.shape:
+        raise ValueError("Images must have same shape.")
+
+    if zeromean:
+        im1 = im1 - (im1[im1==im1].mean())
+        im2 = im2 - (im2[im2==im2].mean())
+
+    if np.any(np.isnan(im1)):
+        im1 = im1.copy()
+        im1[im1!=im1] = 0
+    if np.any(np.isnan(im2)):
+        im2 = im2.copy()
+        im2[im2!=im2] = 0
+
+    fft2,ifft2 = fftn,ifftn = fast_ffts.get_ffts(nthreads=nthreads, use_numpy_fft=use_numpy_fft)
+
+    im1fft = fft2(im1)
+    im2fft = fft2(im2)
+
+    output = dftregistration(im1fft,im2fft,usfac=usfac,
+            return_registered=return_registered, return_error=return_error,
+            zeromean=zeromean, DEBUG=DEBUG, maxoff=maxoff)
+
+    output = [-output[1], -output[0], ] + [o for o in output[2:]]
+
+    if return_registered:
+        output[-1] = np.abs(np.fft.ifftshift(ifft2(output[-1])))
+
+    return output
+
+
+def dftregistration(buf1ft,buf2ft,usfac=1, return_registered=False,
+        return_error=False, zeromean=True, DEBUG=False, maxoff=None,
+        nthreads=1, use_numpy_fft=False):
+    """
+    translated from matlab:
+    http://www.mathworks.com/matlabcentral/fileexchange/18401-efficient-subpixel-image-registration-by-cross-correlation/content/html/efficient_subpixel_registration.html
+
+    Efficient subpixel image registration by crosscorrelation. This code
+    gives the same precision as the FFT upsampled cross correlation in a
+    small fraction of the computation time and with reduced memory 
+    requirements. It obtains an initial estimate of the crosscorrelation peak
+    by an FFT and then refines the shift estimation by upsampling the DFT
+    only in a small neighborhood of that estimate by means of a 
+    matrix-multiply DFT. With this procedure all the image points are used to
+    compute the upsampled crosscorrelation.
+    Manuel Guizar - Dec 13, 2007
+
+    Portions of this code were taken from code written by Ann M. Kowalczyk 
+    and James R. Fienup. 
+    J.R. Fienup and A.M. Kowalczyk, "Phase retrieval for a complex-valued 
+    object by using a low-resolution image," J. Opt. Soc. Am. A 7, 450-458 
+    (1990).
+
+    Citation for this algorithm:
+    Manuel Guizar-Sicairos, Samuel T. Thurman, and James R. Fienup, 
+    "Efficient subpixel image registration algorithms," Opt. Lett. 33, 
+    156-158 (2008).
+
+    Inputs
+    buf1ft    Fourier transform of reference image, 
+           DC in (1,1)   [DO NOT FFTSHIFT]
+    buf2ft    Fourier transform of image to register, 
+           DC in (1,1) [DO NOT FFTSHIFT]
+    usfac     Upsampling factor (integer). Images will be registered to 
+           within 1/usfac of a pixel. For example usfac = 20 means the
+           images will be registered within 1/20 of a pixel. (default = 1)
+
+    Outputs
+    output =  [error,diffphase,net_row_shift,net_col_shift]
+    error     Translation invariant normalized RMS error between f and g
+    diffphase     Global phase difference between the two images (should be
+               zero if images are non-negative).
+    net_row_shift net_col_shift   Pixel shifts between images
+    Greg      (Optional) Fourier transform of registered version of buf2ft,
+           the global phase difference is compensated for.
+    """
+
+    # this function is translated from matlab, so I'm just going to pretend
+    # it is matlab/pylab
+    from numpy import conj,abs,arctan2,sqrt,real,imag,shape,zeros,trunc,ceil,floor,fix
+    from numpy.fft import fftshift,ifftshift
+    fft2,ifft2 = fftn,ifftn = fast_ffts.get_ffts(nthreads=nthreads, use_numpy_fft=use_numpy_fft)
+
+    # Compute error for no pixel shift
+    if usfac == 0:
+        raise ValueError("Upsample Factor must be >= 1")
+        CCmax = sum(sum(buf1ft * conj(buf2ft))); 
+        rfzero = sum(abs(buf1ft)**2);
+        rgzero = sum(abs(buf2ft)**2); 
+        error = 1.0 - CCmax * conj(CCmax)/(rgzero*rfzero); 
+        error = sqrt(abs(error));
+        diffphase=arctan2(imag(CCmax),real(CCmax)); 
+        output=[error,diffphase];
+            
+    # Whole-pixel shift - Compute crosscorrelation by an IFFT and locate the
+    # peak
+    elif usfac == 1:
+        [m,n]=shape(buf1ft);
+        CC = ifft2(buf1ft * conj(buf2ft));
+        if maxoff is None:
+            rloc,cloc = np.unravel_index(abs(CC).argmax(), CC.shape)
+            CCmax=CC[rloc,cloc]; 
+        else:
+            # set the interior of the shifted array to zero
+            # (i.e., ignore it)
+            CC[maxoff:-maxoff,:] = 0
+            CC[:,maxoff:-maxoff] = 0
+            rloc,cloc = np.unravel_index(abs(CC).argmax(), CC.shape)
+            CCmax=CC[rloc,cloc]; 
+        rfzero = sum(abs(buf1ft)**2)/(m*n);
+        rgzero = sum(abs(buf2ft)**2)/(m*n); 
+        error = 1.0 - CCmax * conj(CCmax)/(rgzero*rfzero);
+        error = sqrt(abs(error));
+        diffphase=arctan2(imag(CCmax),real(CCmax)); 
+        md2 = fix(m/2); 
+        nd2 = fix(n/2);
+        if rloc > md2:
+            row_shift = rloc - m;
+        else:
+            row_shift = rloc;
+
+        if cloc > nd2:
+            col_shift = cloc - n;
+        else:
+            col_shift = cloc;
+        #output=[error,diffphase,row_shift,col_shift];
+        output=[row_shift,col_shift]
+        
+    # Partial-pixel shift
+    else:
+        
+        if DEBUG: import pylab
+        # First upsample by a factor of 2 to obtain initial estimate
+        # Embed Fourier data in a 2x larger array
+        [m,n]=shape(buf1ft);
+        mlarge=m*2;
+        nlarge=n*2;
+        CClarge=zeros([mlarge,nlarge], dtype='complex');
+        #CClarge[m-fix(m/2):m+fix((m-1)/2)+1,n-fix(n/2):n+fix((n-1)/2)+1] = fftshift(buf1ft) * conj(fftshift(buf2ft));
+        CClarge[round(mlarge/4.):round(mlarge/4.*3),round(nlarge/4.):round(nlarge/4.*3)] = fftshift(buf1ft) * conj(fftshift(buf2ft));
+        # note that matlab uses fix which is trunc... ?
+      
+        # Compute crosscorrelation and locate the peak 
+        CC = ifft2(ifftshift(CClarge)); # Calculate cross-correlation
+        if maxoff is None:
+            rloc,cloc = np.unravel_index(abs(CC).argmax(), CC.shape)
+            CCmax=CC[rloc,cloc]; 
+        else:
+            # set the interior of the shifted array to zero
+            # (i.e., ignore it)
+            CC[maxoff:-maxoff,:] = 0
+            CC[:,maxoff:-maxoff] = 0
+            rloc,cloc = np.unravel_index(abs(CC).argmax(), CC.shape)
+            CCmax=CC[rloc,cloc]; 
+
+        if DEBUG:
+            pylab.figure(1)
+            pylab.clf()
+            pylab.subplot(131)
+            pylab.imshow(real(CC)); pylab.title("Cross-Correlation (upsampled 2x)")
+            pylab.subplot(132)
+            ups = dftups((buf1ft) * conj((buf2ft)),mlarge,nlarge,2,0,0); pylab.title("dftups upsampled 2x")
+            pylab.imshow(real(((ups))))
+            pylab.subplot(133)
+            pylab.imshow(real(CC)/real(ups)); pylab.title("Ratio upsampled/dftupsampled")
+            print "Upsample by 2 peak: ",rloc,cloc," using dft version: ",np.unravel_index(abs(ups).argmax(), ups.shape)
+            #print np.unravel_index(ups.argmax(),ups.shape)
+        
+        # Obtain shift in original pixel grid from the position of the
+        # crosscorrelation peak 
+        [m,n] = shape(CC); md2 = trunc(m/2); nd2 = trunc(n/2);
+        if rloc > md2 :
+            row_shift2 = rloc - m;
+        else:
+            row_shift2 = rloc;
+        if cloc > nd2:
+            col_shift2 = cloc - n;
+        else:
+            col_shift2 = cloc;
+        row_shift2=row_shift2/2.;
+        col_shift2=col_shift2/2.;
+        if DEBUG: print "row_shift/col_shift from ups2: ",row_shift2,col_shift2
+
+        # If upsampling > 2, then refine estimate with matrix multiply DFT
+        if usfac > 2:
+            #%% DFT computation %%%
+            # Initial shift estimate in upsampled grid
+            zoom_factor=1.5
+            if DEBUG: print row_shift2, col_shift2
+            row_shift0 = round(row_shift2*usfac)/usfac; 
+            col_shift0 = round(col_shift2*usfac)/usfac;     
+            dftshift = trunc(ceil(usfac*zoom_factor)/2); #% Center of output array at dftshift+1
+            if DEBUG: print 'dftshift,rs,cs,zf:',dftshift, row_shift0, col_shift0, usfac*zoom_factor
+            # Matrix multiply DFT around the current shift estimate
+            roff = dftshift-row_shift0*usfac
+            coff = dftshift-col_shift0*usfac
+            upsampled = dftups(
+                    (buf2ft * conj(buf1ft)),
+                    ceil(usfac*zoom_factor),
+                    ceil(usfac*zoom_factor), 
+                    usfac, 
+                    roff,
+                    coff)
+            #CC = conj(dftups(buf2ft.*conj(buf1ft),ceil(usfac*1.5),ceil(usfac*1.5),usfac,...
+            #    dftshift-row_shift*usfac,dftshift-col_shift*usfac))/(md2*nd2*usfac^2);
+            CC = conj(upsampled)/(md2*nd2*usfac**2);
+            if DEBUG:
+                pylab.figure(2)
+                pylab.clf()
+                pylab.subplot(221)
+                pylab.imshow(abs(upsampled)); pylab.title('upsampled')
+                pylab.subplot(222)
+                pylab.imshow(abs(CC)); pylab.title('CC upsampled')
+                pylab.subplot(223); pylab.imshow(np.abs(np.fft.fftshift(np.fft.ifft2(buf2ft * conj(buf1ft))))); pylab.title('xc')
+                yy,xx = np.indices([m*usfac,n*usfac],dtype='float')
+                pylab.contour(yy/usfac/2.-0.5+1,xx/usfac/2.-0.5-1, np.abs(dftups((buf2ft*conj(buf1ft)),m*usfac,n*usfac,usfac)))
+                pylab.subplot(224); pylab.imshow(np.abs(dftups((buf2ft*conj(buf1ft)),ceil(usfac*zoom_factor),ceil(usfac*zoom_factor),usfac))); pylab.title('unshifted ups')
+            # Locate maximum and map back to original pixel grid 
+            rloc,cloc = np.unravel_index(abs(CC).argmax(), CC.shape) 
+            rloc0,cloc0 = np.unravel_index(abs(CC).argmax(), CC.shape) 
+            CCmax = CC[rloc,cloc]
+            #[max1,loc1] = CC.max(axis=0), CC.argmax(axis=0)
+            #[max2,loc2] = max1.max(),max1.argmax()
+            #rloc=loc1[loc2];
+            #cloc=loc2;
+            #CCmax = CC[rloc,cloc];
+            rg00 = dftups(buf1ft * conj(buf1ft),1,1,usfac)/(md2*nd2*usfac**2);
+            rf00 = dftups(buf2ft * conj(buf2ft),1,1,usfac)/(md2*nd2*usfac**2);  
+            #if DEBUG: print rloc,row_shift,cloc,col_shift,dftshift
+            rloc = rloc - dftshift #+ 1 # +1 # questionable/failed hack + 1;
+            cloc = cloc - dftshift #+ 1 # -1 # questionable/failed hack - 1;
+            #if DEBUG: print rloc,row_shift,cloc,col_shift,dftshift
+            row_shift = row_shift0 + rloc/usfac;
+            col_shift = col_shift0 + cloc/usfac;    
+            #if DEBUG: print rloc/usfac,row_shift,cloc/usfac,col_shift
+            if DEBUG: print "Off by: ",(0.25 - float(rloc)/usfac)*usfac , (-0.25 - float(cloc)/usfac)*usfac 
+            if DEBUG: print "correction was: ",rloc/usfac, cloc/usfac
+            if DEBUG: print "Coordinate went from",row_shift2,col_shift2,"to",row_shift0,col_shift0,"to", row_shift, col_shift
+            if DEBUG: print "dftsh - usfac:", dftshift-usfac
+            if DEBUG: print  rloc,cloc,row_shift,col_shift,CCmax,dftshift,rloc0,cloc0
+
+        # If upsampling = 2, no additional pixel shift refinement
+        else:    
+            rg00 = sum(sum( buf1ft * conj(buf1ft) ))/m/n;
+            rf00 = sum(sum( buf2ft * conj(buf2ft) ))/m/n;
+            row_shift = row_shift2
+            col_shift = col_shift2
+        error = 1.0 - CCmax * conj(CCmax)/(rg00*rf00);
+        error = sqrt(abs(error));
+        diffphase=arctan2(imag(CCmax),real(CCmax));
+        # If its only one row or column the shift along that dimension has no
+        # effect. We set to zero.
+        if md2 == 1:
+            row_shift = 0;
+        if nd2 == 1:
+            col_shift = 0;
+        #output=[error,diffphase,row_shift,col_shift];
+        output=[row_shift,col_shift]
+
+    if return_error:
+        # simple estimate of the precision of the fft approach
+        output += [1./usfac,1./usfac]
+
+    # Compute registered version of buf2ft
+    if (return_registered):
+        if (usfac > 0):
+            nr,nc=shape(buf2ft);
+            Nr = np.fft.ifftshift(np.linspace(-np.fix(nr/2),np.ceil(nr/2)-1,nr))
+            Nc = np.fft.ifftshift(np.linspace(-np.fix(nc/2),np.ceil(nc/2)-1,nc))
+            [Nc,Nr] = meshgrid(Nc,Nr);
+            Greg = buf2ft * exp(1j*2*pi*(-row_shift*Nr/nr-col_shift*Nc/nc));
+            Greg = Greg*exp(1j*diffphase);
+        elif (usfac == 0):
+            Greg = buf2ft*exp(1j*diffphase);
+        output.append(Greg)
+
+    return output
+
+
+
