1f76e569f964a1303d4d310744ebef3173fb004b
Adam <keflavich@gmail.com>
2012-09-08 12:45:59 -0600

    added Mock for use with rtfd

--- doc/conf.py
+++ doc/conf.py
@@ -248,3 +248,27 @@ todo_include_todos = True
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+
+# read the docs mocks
+import sys
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['numpy']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
