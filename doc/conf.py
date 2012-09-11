f1420bc6654456c35dded46f3c038391b6b506df
Adam <keflavich@gmail.com>
2012-09-10 22:39:36 -0600

    conf updates - more mockery

--- doc/conf.py
+++ doc/conf.py
@@ -282,6 +282,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy']
+MOCK_MODULES = ['numpy','pyfits','astropy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
