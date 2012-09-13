5b57980819caa3f727efa33a71fe59e6c2910a1b
Adam Ginsburg <adam.g.ginsburg@gmail.com>
2012-09-13 13:21:31 -0600

    mock out numpy...

--- doc/conf.py
+++ doc/conf.py
@@ -294,6 +294,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['scipy','pyfits','astropy']
+MOCK_MODULES = ['numpy','scipy','pyfits','astropy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
