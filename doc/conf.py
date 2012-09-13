b75613494279a4c195ce2ee6922d2762717a2154
Adam Ginsburg <adam.g.ginsburg@gmail.com>
2012-09-13 13:26:06 -0600

    mock out pytest...

--- doc/conf.py
+++ doc/conf.py
@@ -294,6 +294,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy','scipy','pyfits','astropy']
+MOCK_MODULES = ['numpy','scipy','pyfits','astropy','pytest']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
