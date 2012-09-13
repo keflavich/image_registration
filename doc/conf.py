1b61dbe25405487ebcf7c8bfdfdf8a8f5b31f946
Adam Ginsburg <adam.g.ginsburg@gmail.com>
2012-09-13 13:30:24 -0600

    mock out so many things...

--- doc/conf.py
+++ doc/conf.py
@@ -294,6 +294,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy','scipy','pyfits','astropy','pytest']
+MOCK_MODULES = ['numpy', 'scipy', 'pyfits', 'astropy', 'pytest', 'astropy.wcs',
+    'astropy.io', 'astropy.io.fits', 'scipy.ndimage']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
