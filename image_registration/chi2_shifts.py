fixed examples in chi2_shifts fixed sign bug in shift.py

image_registration/chi2_shifts.py
@@ -68,19 +68,30 @@ def chi2_shift(im1, im2, err=None, upsample_factor=10, boundary='wrap',
     Returns
     -------
     dx,dy : float,float
-        .. todo:: CHECK THIS 
-        .. todo:: make upsample_factor only zoom on the chi2-relevant region
         Measures the amount im2 is offset from im1 (i.e., shift im2 by -1 *
         these #'s to match im1)
 
+        .. todo:: CHECK THIS 
+
     Examples
     --------
-    # Create a 2d array
+    >>> # Create a 2d array
     >>> image = np.random.randn(50,55)
-    # shift it in both directions
-    >>> shifted = np.roll(np.roll(image,0,12),1,5)
-    # determine shift
-    >>> dx,dy = chi2shift(image, shifted, upsample_factor='auto')
+    >>> # shift it in both directions
+    >>> shifted = np.roll(np.roll(image,12,0),5,1)
+    >>> # determine shift
+    >>> import image_registration
+    >>> dx,dy,edx,edy = image_registration.chi2_shift(image, shifted, upsample_factor='auto')
+    >>> # Check that the shift is correct
+    >>> print "dx - fitted dx = ",dx-5," error: ",edx
+    >>> print "dy - fitted dy = ",dy-12," error: ",edy
+    >>> # that example was boring; instead let's do one with a non-int shift
+    >>> shifted2 = image_registration.fft_tools.shift(image,3.665,-4.25)
+    >>> dx2,dy2,edx2,edy2 = image_registration.chi2_shift(image, shifted2, upsample_factor='auto')
+    >>> print "dx - fitted dx = ",dx2-3.665," error: ",edx2
+    >>> print "dy - fitted dy = ",dy2-(-4.25)," error: ",edy2
+    
+    .. todo:: understand numerical error in fft-shifted version
 
     """
     if not im1.shape == im2.shape:

image_registration/cross_correlation_shifts.py
@@ -62,8 +62,8 @@ def cross_correlation_shifts(image1, image2, errim1=None, errim2=None,
     >>> im2 = np.zeros([10,10])
     >>> im1[4,3] = 1
     >>> im2[5,5] = 1
-    >>> import AG_image_tools
-    >>> yoff,xoff = AG_image_tools.cross_correlation_shifts(im1,im2)
+    >>> import image_registration
+    >>> yoff,xoff = image_registration.cross_correlation_shifts(im1,im2)
     >>> im1_aligned_to_im2 = np.roll(np.roll(im1,int(yoff),1),int(xoff),0)
     >>> assert (im1_aligned_to_im2-im2).sum() == 0
     

image_registration/fft_tools/shift.py
@@ -2,7 +2,7 @@ import fast_ffts
 import numpy as np
 
 def shift(data, deltax, deltay, phase=0, nthreads=1, use_numpy_fft=False,
-        return_abs=True):
+        return_abs=False, return_real=True):
     """
     FFT-based sub-pixel image shift
     http://www.mathworks.com/matlabcentral/fileexchange/18401-efficient-subpixel-image-registration-by-cross-correlation/content/html/efficient_subpixel_registration.html
@@ -19,7 +19,9 @@ def shift(data, deltax, deltay, phase=0, nthreads=1, use_numpy_fft=False,
     Ny = np.fft.ifftshift(np.linspace(-np.fix(ny/2),np.ceil(ny/2)-1,ny))
     Nx,Ny = np.meshgrid(Nx,Ny)
     gg = ifftn( fftn(data)* np.exp(1j*2*np.pi*(-deltax*Nx/nx-deltay*Ny/ny)) * np.exp(-1j*phase) )
-    if return_abs:
+    if return_real:
+        return np.real(gg)
+    elif return_abs:
         return np.abs(gg)
     else:
         return gg