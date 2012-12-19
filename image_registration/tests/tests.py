bfd1c32126d3a9b320a307bd96d60ce6907b3b93
Adam <keflavich@gmail.com>
2012-12-19 09:23:13 -0700

    added some test codes intended for interactive (not py.test) use

---
+++ image_registration/tests/tests.py
@@ -0,0 +1,8 @@
+from image_registration import *
+from image_registration.tests import *
+
+im1a = make_extended(100,200)
+im2a = make_offset_extended(im1a, 4.76666, -12.33333333333333333333333, noise=0)
+offsets = chi2_shift(im1a,im2a,err=im2a*0+0.1,upsample_factor='auto')
+print offsets
+
