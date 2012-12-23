7a755fbf93a3217bc22c049ad4e5daa2339d18a8
Adam <keflavich@gmail.com>
2012-12-23 00:27:17 -0700

    added benchmark tests

---
+++ examples/benchmarks_shift.py
@@ -0,0 +1,106 @@
+"""
+    imsize   map_coordinates    fourier_shift
+        50          0.016211       0.00944495
+        84         0.0397182        0.0161059
+       118          0.077543        0.0443089
+       153          0.132948         0.058187
+       187          0.191808        0.0953341
+       221          0.276543          0.12069
+       255          0.357552         0.182863
+       289          0.464547          0.26451
+       324          0.622776         0.270612
+       358          0.759015         0.713239
+       392          0.943339         0.441262
+       426           1.12885         0.976379
+       461           1.58367          1.26116
+       495           1.62482         0.824757
+       529           1.83506          1.19455
+       563           3.21001          2.82487
+       597           2.64892          2.23473
+       632           2.74313          2.21019
+       666           3.07002          2.49054
+       700           3.50138          1.59507
+
+Fourier outperforms map_coordinates slightly.  It wraps, though, while
+map_coordinates in general does not.
+"""
+import itertools
+import timeit
+import time
+
+import numpy as np
+
+timings = {'map_coordinates':[],
+           'fourier_shift':[],
+           #'griddata_nearest':[],
+           #'griddata_linear':[],
+           #'griddata_cubic':[],
+           }
+
+imsizes = np.round(np.linspace(50,1024,20))
+imsizes = np.round(np.linspace(50,700,20)) # just playing around with what's reasonable for my laptop
+
+for imsize in imsizes:
+    t0 = time.time()
+    setup = """
+    import numpy as np
+    #im = np.random.randn({imsize},{imsize})
+    yy,xx = np.indices([{imsize},{imsize}])
+    im = np.exp(-((xx-{imsize}/2.)**2+(yy-{imsize}/2.)**2)/(2**2*2.))
+    yr = np.arange({imsize})
+    xr = np.arange({imsize})
+    import image_registration.fft_tools.shift as fsh
+    import image_registration.fft_tools.zoom as fzm
+    import scipy.interpolate as si
+    import scipy.ndimage as snd
+    points = zip(xx.flat,yy.flat)
+    imflat = im.ravel()
+    """.replace("    ","").format(imsize=imsize)
+
+    fshift_timer = timeit.Timer("ftest=fsh.shiftnd(im,(0.5,0.5))",
+            setup=setup)
+
+    # too slow!
+    # interp2d_timer = timeit.Timer("itest=si.interp2d(xr,yr,im)(xr-0.5,yr-0.5)",
+    #        setup=setup)
+
+    mapcoord_timer = timeit.Timer("mtest=snd.map_coordinates(im,[yy-0.5,xx-0.5])",
+            setup=setup)
+
+    # all slopw
+    #grid_timer_nearest = timeit.Timer("gtest=si.griddata(points,imflat,(xx-0.5,yy-0.5), method='nearest')",
+    #        setup=setup)
+    #grid_timer_linear = timeit.Timer("gtest=si.griddata(points,imflat,(xx-0.5,yy-0.5), method='linear')",
+    #        setup=setup)
+    #grid_timer_cubic = timeit.Timer("gtest=si.griddata(points,imflat,(xx-0.5,yy-0.5), method='cubic')",
+    #        setup=setup)
+
+    print "imsize %i fourier shift " % imsize,
+    timings['fourier_shift'].append( np.min(fshift_timer.repeat(3,10)) )
+    print "imsize %i map_coordinates shift " % imsize,
+    timings['map_coordinates'].append( np.min(mapcoord_timer.repeat(3,10)) )
+    #timings['griddata_nearest'].append( np.min(grid_timer_nearest.repeat(3,10)) )
+    #timings['griddata_linear'].append( np.min(grid_timer_linear.repeat(3,10)) )
+    #timings['griddata_cubic'].append( np.min(grid_timer_cubic.repeat(3,10)) )
+    print "imsize %i done, %f seconds" % (imsize,time.time()-t0)
+
+
+print "%10s " % "imsize"," ".join(["%16s" % t for t in timings.keys()])
+for ii,sz in enumerate(imsizes):
+    print "%10i " % sz," ".join(["%16.6g" % t[ii] for t in timings.values()])
+
+
+import scipy.optimize as scopt
+
+def f(x,a,b,c):
+    return c+a*x**b
+
+pm,err = scopt.curve_fit(f,imsizes[2:],timings['map_coordinates'][2:])
+pf,err = scopt.curve_fit(f,imsizes[2:],timings['fourier_shift'][2:])
+
+import matplotlib.pyplot as pl
+pl.clf()
+pl.loglog(imsizes,timings['map_coordinates'],'+')
+pl.loglog(imsizes,timings['fourier_shift'],'x')
+pl.loglog(imsizes,f(imsizes,*pm))
+pl.loglog(imsizes,f(imsizes,*pf))
---
+++ examples/benchmarks_zoom.py
@@ -0,0 +1,116 @@
+"""
+Benchmark tests comparing fourier zoom to other techniques
+
+    imsize   map_coordinates     fourier_zoom
+        50         0.0523551        0.0214219
+       101          0.214316         0.092334
+       153          0.499386         0.245633
+       204           1.13154         0.506588
+       255           2.03562          1.33743
+       306            4.2135          3.11828
+       358           6.59853          5.83727
+       409           8.52711          9.29976
+       460           11.1365          12.3836
+       511           17.8496          17.9716
+       563           19.4098          24.3088
+       614            10.559          29.7606
+       665           11.6219          15.7843
+       716           15.1977          20.9938
+       768           16.6483          22.0162
+       819           19.7834          28.2969
+       870           21.6399          34.0512
+       921           24.7352          41.2564
+       973           27.3512          48.8034
+      1024           32.1365          51.9153
+
+Somewhat disappointing, but the fourier approach can be parallelized pretty
+easily, I don't know about map_coordinates.
+
+Also, I'm more than a little suspicious of these results; the execution times
+took a precipitous drop between 615 and 665 seconds, which I think is an
+indication that I was running too much other junk in parallel.
+
+It's also pretty weird that the fourier approach seems to go up more steeply
+than the map_coordinates approach; it could indicate memory limitations on my
+machine.
+
+"""
+import itertools
+import timeit
+import time
+
+import numpy as np
+
+zoomtimings = {'map_coordinates':[],
+           'fourier_zoom':[],
+           #'griddata_nearest':[],
+           #'griddata_linear':[],
+           #'griddata_cubic':[],
+           }
+
+imsizes = np.round(np.linspace(50,1024,20))
+
+for imsize in imsizes:
+    t0 = time.time()
+    setup = """
+    import numpy as np
+    #im = np.random.randn({imsize},{imsize})
+    yy,xx = np.indices([{imsize},{imsize}])
+    im = np.exp(-((xx-{imsize}/2.)**2+(yy-{imsize}/2.)**2)/(2**2*2.))
+    # upsample by factor of 2
+    yr = np.linspace(0,(({imsize}*2)-1)/2.,{imsize}*2)-0.25 # middle conventions...
+    xr = np.linspace(0,(({imsize}*2)-1)/2.,{imsize}*2)-0.25 # middle conventions...
+    xxnew,yynew = np.meshgrid(xr,yr)
+    import image_registration.fft_tools.zoom as fzm
+    import scipy.interpolate as si
+    import scipy.ndimage as snd
+    points = zip(xx.flat,yy.flat)
+    imflat = im.ravel()
+    """.replace("    ","").format(imsize=imsize)
+
+    fzoom_timer = timeit.Timer("ftest=fzm.zoomnd(im,usfac=2,outshape=xxnew.shape)",
+            setup=setup)
+
+    # too slow!
+    # interp2d_timer = timeit.Timer("itest=si.interp2d(xr,yr,im)(xr-0.5,yr-0.5)",
+    #        setup=setup)
+
+    mapzoom_timer = timeit.Timer("mtest=snd.map_coordinates(im,[yynew,xxnew])",
+            setup=setup)
+
+    # all slopw
+    #grid_timer_nearest = timeit.Timer("gtest=si.griddata(points,imflat,(xx-0.5,yy-0.5), method='nearest')",
+    #        setup=setup)
+    #grid_timer_linear = timeit.Timer("gtest=si.griddata(points,imflat,(xx-0.5,yy-0.5), method='linear')",
+    #        setup=setup)
+    #grid_timer_cubic = timeit.Timer("gtest=si.griddata(points,imflat,(xx-0.5,yy-0.5), method='cubic')",
+    #        setup=setup)
+
+    print "imsize %i fourier zoom " % imsize,
+    zoomtimings['fourier_zoom'].append( np.min(fzoom_timer.repeat(3,10)) )
+    print "imsize %i map_coordinates zoom " % imsize,
+    zoomtimings['map_coordinates'].append( np.min(mapzoom_timer.repeat(3,10)) )
+    #zoomtimings['griddata_nearest'].append( np.min(grid_timer_nearest.repeat(3,10)) )
+    #zoomtimings['griddata_linear'].append( np.min(grid_timer_linear.repeat(3,10)) )
+    #zoomtimings['griddata_cubic'].append( np.min(grid_timer_cubic.repeat(3,10)) )
+    print "imsize %i done, %f seconds" % (imsize,time.time()-t0)
+
+print "%10s " % "imsize"," ".join(["%16s" % t for t in zoomtimings.keys()])
+for ii,sz in enumerate(imsizes):
+    print "%10i " % sz," ".join(["%16.6g" % t[ii] for t in zoomtimings.values()])
+
+
+import scipy.optimize as scopt
+
+def f(x,a,b):
+    return a*x**b
+
+pm,err = scopt.curve_fit(f,imsizes[1:],zoomtimings['map_coordinates'][1:])
+pf,err = scopt.curve_fit(f,imsizes[1:],zoomtimings['fourier_zoom'][1:])
+
+import matplotlib.pyplot as pl
+pl.clf()
+pl.loglog(imsizes,zoomtimings['map_coordinates'],'+')
+pl.loglog(imsizes,zoomtimings['fourier_zoom'],'x')
+pl.loglog(imsizes,f(imsizes,*pm))
+pl.loglog(imsizes,f(imsizes,*pf))
