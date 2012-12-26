3468c80c3c515defa8c08756153cd7179eafba97
Adam Ginsburg <keflavich@gmail.com>
2012-12-26 12:00:54 -0700

    added it to init now

--- image_registration/__init__.py
+++ image_registration/__init__.py
@@ -1,5 +1,5 @@
 from cross_correlation_shifts import cross_correlation_shifts, cross_correlation_shifts_FITS
-from chi2_shifts import chi2_shift,chi2n_map
+from chi2_shifts import chi2_shift,chi2n_map,chi2_shift_iterzoom
 from register_images import register_images
 import fft_tools
 import tests
