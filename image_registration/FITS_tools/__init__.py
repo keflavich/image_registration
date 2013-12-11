eac2dd4006512503ded064b5a21dab875a8427db
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-12-11 14:55:01 +0100

    udpate init

--- image_registration/FITS_tools/__init__.py
+++ image_registration/FITS_tools/__init__.py
@@ -1,3 +1,3 @@
 from match_images import project_to_header,match_fits,register_fits
 from fits_overlap import fits_overlap,header_overlap
-from hcongrid import hcongrid
+from hcongrid import hcongrid,zoom_fits
