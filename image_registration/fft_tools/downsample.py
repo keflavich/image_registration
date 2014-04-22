e411886cea7bd150be9b626f580f7916acea97b9
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-04-22 10:54:11 -0400

    add downsample_along_axis tool to the downsample code

--- image_registration/fft_tools/downsample.py
+++ image_registration/fft_tools/downsample.py
@@ -1,15 +1,11 @@
-import numpy
-try:
-    from scipy.stats import nanmean as mean
-except ImportError:
-    from numpy import mean
+import numpy as np
 
-def downsample(myarr,factor,estimator=mean):
+def downsample(myarr,factor,estimator=np.nanmean):
     """
     Downsample a 2D array by averaging over *factor* pixels in each axis.
     Crops upper edge if the shape is not a multiple of factor.
 
-    This code is pure numpy and should be fast.
+    This code is pure np and should be fast.
 
     keywords:
         estimator - default to mean.  You can downsample by summing or
@@ -18,7 +14,7 @@ def downsample(myarr,factor,estimator=mean):
     """
     ys,xs = myarr.shape
     crarr = myarr[:ys-(ys % int(factor)),:xs-(xs % int(factor))]
-    dsarr = estimator( numpy.concatenate([[crarr[i::factor,j::factor] 
+    dsarr = estimator( np.concatenate([[crarr[i::factor,j::factor] 
         for i in range(factor)] 
         for j in range(factor)]), axis=0)
     return dsarr
@@ -31,18 +27,18 @@ def downsample_cube(myarr,factor,ignoredim=0):
     if ignoredim > 0: myarr = myarr.swapaxes(0,ignoredim)
     zs,ys,xs = myarr.shape
     crarr = myarr[:,:ys-(ys % int(factor)),:xs-(xs % int(factor))]
-    dsarr = mean(numpy.concatenate([[crarr[:,i::factor,j::factor] 
+    dsarr = mean(np.concatenate([[crarr[:,i::factor,j::factor] 
         for i in range(factor)] 
         for j in range(factor)]), axis=0)
     if ignoredim > 0: dsarr = dsarr.swapaxes(0,ignoredim)
     return dsarr
 
-def downsample_1d(myarr,factor,estimator=mean):
+def downsample_1d(myarr,factor,estimator=np.nanmean):
     """
     Downsample a 1D array by averaging over *factor* pixels.
     Crops right side if the shape is not a multiple of factor.
 
-    This code is pure numpy and should be fast.
+    This code is pure np and should be fast.
 
     keywords:
         estimator - default to mean.  You can downsample by summing or
@@ -51,7 +47,48 @@ def downsample_1d(myarr,factor,estimator=mean):
     """
     xs = myarr.shape
     crarr = myarr[:xs-(xs % int(factor))]
-    dsarr = estimator( numpy.concatenate([[crarr[i::factor] 
+    dsarr = estimator( np.concatenate([[crarr[i::factor] 
         for i in range(factor)] ]),axis=0)
     return dsarr
 
+def downsample_axis(myarr, factor, axis, estimator=np.nanmean, truncate=False):
+    """
+    Downsample an ND array by averaging over *factor* pixels along an axis.
+    Crops right side if the shape is not a multiple of factor.
+
+    This code is pure np and should be fast.
+
+    keywords:
+        estimator - default to mean.  You can downsample by summing or
+            something else if you want a different estimator
+            (e.g., downsampling error: you want to sum & divide by sqrt(n))
+    """
+    # size of the dimension of interest
+    xs = myarr.shape[axis]
+    
+    if xs % int(factor) != 0:
+        if truncate:
+            view = [slice(None) for ii in range(myarr.ndim)]
+            view[axis] = slice(None,xs-(xs % int(factor)))
+            crarr = myarr[view]
+        else:
+            newshape = list(myarr.shape)
+            newshape[axis] = (factor - xs % int(factor))
+            extension = np.empty(newshape) * np.nan
+            crarr = np.concatenate((myarr,extension), axis=axis)
+    else:
+        crarr = myarr
+
+    def makeslice(startpoint,axis=axis,step=factor):
+        # make empty slices
+        view = [slice(None) for ii in range(myarr.ndim)]
+        # then fill the appropriate slice
+        view[axis] = slice(startpoint,None,step)
+        return view
+
+    # The extra braces here are crucial: We're adding an extra dimension so we
+    # can average across it!
+    stacked_array = np.concatenate([[crarr[makeslice(ii)]] for ii in range(factor)])
+
+    dsarr = estimator(stacked_array, axis=0)
+    return dsarr
