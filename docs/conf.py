6c39874400a5d238977752d4ed54e7b35084ca1d
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2022-09-23 11:29:43 -0400

    remove check for 'release' attribute

--- docs/conf.py
+++ docs/conf.py
@@ -150,10 +150,10 @@ if eval(setup_cfg.get('edit_on_github')):
 
     versionmod = __import__(setup_cfg['package_name'] + '.version')
     edit_on_github_project = setup_cfg['github_project']
-    if versionmod.version.release:
-        edit_on_github_branch = "v" + versionmod.version.version
-    else:
-        edit_on_github_branch = "master"
+    # FAILS: if versionmod.version.release:
+    # FAILS:     edit_on_github_branch = "v" + versionmod.version.version
+    # FAILS: else:
+    edit_on_github_branch = "master"
 
     edit_on_github_source_root = ""
     edit_on_github_doc_root = "docs"
